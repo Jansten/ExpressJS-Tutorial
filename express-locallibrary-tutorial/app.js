09c25e8739c262c4a4bd8e31272e07713857afd4
Michael Hannon <bostonhannon@gmail.com>
2019-07-01 09:12:10 -0700

    Update app.js
    Seriously, this username and password don't work, don't evene bother.

--- express-locallibrary-tutorial/app.js
+++ express-locallibrary-tutorial/app.js
@@ -9,6 +9,13 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+//Set up mongoose connection
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb+srv://libraryUser:libraryPassword@cluster0-hn18x.mongodb.net/local_library?retryWrites=true&w=majority'; // This username and password doesn't work, don't bother trying
+mongoose.connect(mongoDB, { useNewUrlParser: true });
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
